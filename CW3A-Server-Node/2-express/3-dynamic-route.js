ed8dddd724894d2cd423e422e83c42ac68becb1b
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-03-19 21:24:17 +0100

    fix: Format

--- CW3A-Server-Node/2-express/3-dynamic-route.js
+++ CW3A-Server-Node/2-express/3-dynamic-route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get('/cours/:titre/descr', (req, res) => {
-    res.send(`Vous avez demandé le cours ${req.params.titre}`);
+app.get("/cours/:titre/descr", (req, res) => {
+  res.send(`Vous avez demandé le cours ${req.params.titre}`);
 });
 
 app.listen(8080);
