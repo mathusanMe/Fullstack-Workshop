40cd630cb69058076f0641073d7145524b79ce79
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-03-19 16:59:30 +0100

    feat: Add read file sync

--- CW3A-Server-Node/1-node/2-asyncReadFile.js
+++ CW3A-Server-Node/1-node/2-asyncReadFile.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 
-console.log('reading file, please wait');
 
 fs.readFile('sample.txt', 'utf8', function (err, data) {
     if (err) throw err;
     console.log(data);
 });
+
+console.log('reading file, please wait');
