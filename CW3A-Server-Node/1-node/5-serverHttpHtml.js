ba8cc920ffc0ac81159a51bba4cfa39eb3c15f49
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-03-19 17:27:51 +0100

    fix: Format

--- CW3A-Server-Node/1-node/5-serverHttpHtml.js
+++ CW3A-Server-Node/1-node/5-serverHttpHtml.js
@@ -1,23 +1,23 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-let contentType = {'Content-Type': 'text/html; charset=utf-8'};
+let contentType = { "Content-Type": "text/html; charset=utf-8" };
 
 const server = http.createServer(function (req, res) {
-    fs.readFile('sample.txt', 'utf-8', function (err, data) {
-        if (err) {
-            res.writeHead(404, contentType);
-            res.write('Fichier non trouvé');
-            res.end();
-        } else {
-            res.writeHead(200, contentType);
-            res.write('<title>Newer Sample Text</title>');
-            res.write('<h1>Sample Text</h1>');
-            res.write(`<p>${data}</p>`);
-            res.write('<footer><p>Mathusan Selvakumar</p></footer>');
-            res.end();
-        }
-    });  
+  fs.readFile("sample.txt", "utf-8", function (err, data) {
+    if (err) {
+      res.writeHead(404, contentType);
+      res.write("Fichier non trouvé");
+      res.end();
+    } else {
+      res.writeHead(200, contentType);
+      res.write("<title>Newer Sample Text</title>");
+      res.write("<h1>Sample Text</h1>");
+      res.write(`<p>${data}</p>`);
+      res.write("<footer><p>Mathusan Selvakumar</p></footer>");
+      res.end();
+    }
+  });
 });
 
-server.listen(8080, 'localhost');
+server.listen(8080, "localhost");
